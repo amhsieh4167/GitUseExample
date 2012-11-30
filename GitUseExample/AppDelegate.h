8ee2a253bf74a9c6460afde43433523f6237473a
Alex Hsieh <amhsieh@gmail.com>
2012-11-30 12:39:34 -0600

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Alex Hsieh on 11/30/12.
+//  Copyright (c) 2012 Alex Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Alex Hsieh on 11/30/12.
+//  Copyright (c) 2012 Alex Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
